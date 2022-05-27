b9067e3335e6265c6d2e386c157aa95b7bb8c65c
gokhan.guzelyurt <Gok26han@>
2022-05-27 02:11:59 +0200

    Security

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -5,15 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
     private final SecurityService securityService;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
-    public SecurityConfig(SecurityService securityService) {
+    public SecurityConfig(SecurityService securityService, AuthenticationSuccessHandler authenticationSuccessHandler) {
         this.securityService = securityService;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
     }
 
 
@@ -37,9 +40,9 @@ public class SecurityConfig {
                 .authorizeRequests()
 //                .antMatchers("/user/**").hasRole("Admin")
                 .antMatchers("/user/**").hasAuthority("Admin")
-                .antMatchers("/project/**").hasRole("Manager")
-                .antMatchers("/task/employee/**").hasRole("Employee")
-                .antMatchers("/task/**").hasRole("Manager")
+                .antMatchers("/project/**").hasAuthority("Manager")
+                .antMatchers("/task/employee/**").hasAuthority("Employee")
+                .antMatchers("/task/**").hasAuthority("Manager")
 //                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
 //                .antMatchers("/task/**").hasAnyRole("EMPLOYEE,ADMIN")
                 .antMatchers(
@@ -47,14 +50,15 @@ public class SecurityConfig {
                      "/login",
                      "/fragments/**",
                      "/assets/**",
-                     "images/**"
+                     "/images/**"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
 //                .httpBasic()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/welcome")
+//                    .defaultSuccessUrl("/welcome")
+                    .successHandler(authenticationSuccessHandler)
                     .failureUrl("/login?error=true")
                     .permitAll()
                 .and()
